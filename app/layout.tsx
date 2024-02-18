d44da228d6268ef7544cfd1439659c7209787435
Dmitri Khassanov <khdmitri@yahoo.com>
2024-02-18 15:28:06 +0700

    fixes 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Script src="https://telegram.org/js/telegram-web-app.js" />
       <body className={inter.className}>{children}</body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,52 @@
 import useTelegram from "./(hooks)/useTelegram";
 import {Box, Button, Container, Typography} from "@mui/material";
 import {useState} from "react";
+import Script from "next/script";
+
+function getTelegram(){
+    let w: any = null
+    if (typeof window !== "undefined") {
+        // Client-side-only code
+        w = window
+        console.log("window=", w)
+    }
+    const tg = w?.Telegram?.WebApp
+    console.log("TG=", tg)
+
+    const onClose = () => {
+        tg?.close()
+    }
+
+    const onToggleButton = () => {
+        if (tg?.MainButton?.isVisible) {
+            tg?.MainButton?.hide()
+        } else {
+            tg?.MainButton?.show()
+        }
+    }
+
+
+    return {
+        tg,
+        onClose,
+        onToggleButton,
+        user: tg?.initDataUnsafe?.user,
+    }
+}
 
 export default function Home() {
-    const {user, onClose, onToggleButton} = useTelegram()
+    let {user, onClose, onToggleButton} = getTelegram()
+
+    const refreshTelegram = () => {
+        const tg = getTelegram()
+        user = tg.tg
+        onClose = tg.onClose
+        onToggleButton = tg.onToggleButton
+    }
 
     return (
         <Container>
+            <Script src="https://telegram.org/js/telegram-web-app.js" />
             <Box display="flex" flexDirection="column">
                 <Typography variant="h6">
                     <p>Username: {user?.username}</p>
@@ -16,6 +56,7 @@ export default function Home() {
                     <p>Last Name: {user?.last_name}</p>
                     <p>Is Premium: {user?.is_premium}</p>
                 </Typography>
+                <Button onClick={refreshTelegram}>Refresh</Button>
             </Box>
             <Button onClick={onToggleButton}>Toggle</Button>
             <Button onClick={onClose}>Close Application</Button>
